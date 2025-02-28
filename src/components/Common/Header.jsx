ea0e7067ee567350f8491f02bd5dc75b5bcd3e16
Gurneetsingh07 <gurneetsingh86483@gmail.com>
2025-02-28 13:49:19 +0530

    footer and user layout updated

--- src/components/Common/Header.jsx
+++ src/components/Common/Header.jsx
@@ -2,7 +2,7 @@ import Topbar from "../Layout/Topbar"
 import Navbar from "../Common/Navbar";
 const Header = () => {
   return (
-    <header>
+    <header className="border-b border-gray-200">
       <Topbar/>
       <Navbar/>
     </header>
