26d9d8768aec59d1eb285b4b1d60b0373ceea046
Gurneetsingh07 <gurneetsingh86483@gmail.com>
2025-03-19 04:26:57 +0530

    added admin sidebar and admin dashboard page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserLayout from "./components/Layout/UserLayout";
 import Home from "./pages/Home";
-import {Toaster} from "sonner";
+import { Toaster } from "sonner";
 import Login from "./pages/Login";
-import Register from "./pages/Register"
+import Register from "./pages/Register";
 import Profile from "./pages/Profile";
 import CollectionPage from "./pages/CollectionPage";
 import ProductDetails from "./components/Products/ProductDetails";
@@ -12,26 +12,33 @@ import Checkout from "./components/Cart/Checkout";
 import OrderConfrimationPage from "./pages/OrderConfrimationPage";
 import OrderDetailsPage from "./pages/OrderDetailsPage";
 import MyordersPage from "./pages/MyordersPage";
+import AdminLayout from "./components/Admin/AdminLayout";
+import AdminHomepage from "./pages/AdminHomepage";
 const App = () => {
   return (
     <BrowserRouter>
-    <Toaster position="top-right"/>
+      <Toaster position="top-right" />
       <Routes>
         <Route path="/" element={<UserLayout />}>
           {/*User layout*/}
-          <Route index element={<Home/>}/>
-          <Route path="login" element={<Login />}/>
-          <Route path="register" element={<Register />}/>
-          <Route path="profile" element={<Profile/>}/>
-          <Route path="collections/:collection" element={<CollectionPage/>}/>
-          <Route path="product/:id" element={<ProductDetails/>}/>
-          <Route path="checkout" element={<Checkout/>}/>
-          <Route path="order-confirmation" element={<OrderConfrimationPage/>}/>
-          <Route path="order/:id" element={<OrderDetailsPage/>}/>
-          <Route path="my-orders" element={<MyordersPage/>}/>
-          
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="collections/:collection" element={<CollectionPage />} />
+          <Route path="product/:id" element={<ProductDetails />} />
+          <Route path="checkout" element={<Checkout />} />
+          <Route
+            path="order-confirmation"
+            element={<OrderConfrimationPage />}
+          />
+          <Route path="order/:id" element={<OrderDetailsPage />} />
+          <Route path="my-orders" element={<MyordersPage />} />
         </Route>
         <Route>{/*admin layout*/}</Route>
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route index element={<AdminHomepage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
